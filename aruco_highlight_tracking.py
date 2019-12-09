566c51489414b947d4a36bc7252af9176f82f843
Iyad Aldaqre <iyad@datigrezzi.com>
2019-12-09 09:44:51 +0100

    added comments

--- aruco_highlight_tracking.py
+++ aruco_highlight_tracking.py
@@ -5,9 +5,6 @@ import time
 from distutils import util
 import argparse
 
-# solvePnP tutorial:
-# https://www.learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
-
 parser = argparse.ArgumentParser(description = 'Marker Tracking & Pose Estimation')
 parser.add_argument('--inputVideo', type = str, help = 'Path to the video of the object to be tracked')
 parser.add_argument('--referenceImage', type = str, help = 'Path to an image of the object to track including markers')
@@ -76,47 +73,62 @@ if args.smooth:
     h_array = deque(maxlen = 5)
 
 while(True):
+    # read next frame from VideoCapture
     ret, frame = cap.read()
     if frame is not None:
+        # convert frame to gray scale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect aruco markers in gray frame
         res_corners, res_ids, _ = cv2.aruco.detectMarkers(gray, aruco_dict, parameters = parameters)
-        # if we have markers
+        # if markers were detected
         if res_ids is not None:
+            # find which markers in frame match those in reference image
             idx = which(refIds, res_ids)
-            # if our markers are int he reference image
+            # if any detected marker in frame is also in the reference image
             if len(idx) > 0:
-                # take all markers and reshape the list of corners
+                # flatten the array of corners in the frame and reference image
                 these_res_corners = np.concatenate(res_corners, axis = 1)
                 these_ref_corners = np.concatenate([refCorners[x] for x in idx], axis = 1)
+                # estimate homography matrix
                 h, s = cv2.findHomography(these_ref_corners, these_res_corners, cv2.RANSAC, 5.0)
+                # if we want smoothing
                 if args.smooth:
                     h_array.append(h)
                     this_h = np.mean(h_array, axis = 0)
                 else:
                     this_h = h
+                # transform the rectangle using the homography matrix
                 newRect = cv2.perspectiveTransform(rect, this_h, (gray.shape[1],gray.shape[0]))
+                # draw the rectangle on the frame
                 frame = cv2.polylines(frame, np.int32(newRect), True, (0,0,0), 10)
+                # if we want the pose estimation
                 if args.vector:
-                    # project points onto 2d image
+                    # add a distance estimate to the markers in the reference image (Z axis from camera) - here it is 50
                     these_ref_corners_3d = np.append(these_ref_corners, np.add(np.zeros((1,these_ref_corners.shape[1],1)), 500), axis = 2)
+                    # estimate rotation and translation vectors with solvePnP
                     success, rotation_vector, translation_vector = cv2.solvePnP(these_ref_corners_3d, these_res_corners, camera_matrix, dist_coeffs, flags=cv2.cv2.SOLVEPNP_ITERATIVE)
+                    # project the start and end points of the pose vector from reference image to frame
                     (pose_point_2d, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 0.0), (0.0, 0.0, 500.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
                     p1 = (int(pose_point_2d[0][0][0]), int(pose_point_2d[0][0][1]))
                     p2 = (int(pose_point_2d[1][0][0]), int(pose_point_2d[1][0][1]))
+                    # draw the veector as a line in the frame
                     cv2.line(frame, p1, p2, (0,0,255), 10)
-
-        if len(res_corners) > 0:
+            # draw detected markers in frame with their ids
             cv2.aruco.drawDetectedMarkers(frame,res_corners,res_ids)
-        # Display the resulting frame
+        # if video is to be saved
         if args.outputVideo is not None:
             videoOut.write(frame)
+        # resize frame to half of both axes (because my screen is small!)
         frame = cv2.resize(frame, None, fx = 0.5, fy = 0.5)
+        # Display the resulting frame
         cv2.imshow('frame',frame)
+    # exit if q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# When everything done, release the capture and video output
 if args.outputVideo is not None:
     videoOut.release()
 cap.release()
+# close cv2 window
 cv2.destroyAllWindows()
